5467e1ef54b4d755619a244031eac29ab0d55905
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-05-02 16:37:34 +0200

    travis-bug: reduced test datasets Correted the clean rule
    Former-commit-id: 0fee6500c4c27767cc36f72a9e80984228c199a3 [formerly babc0534f293761cfb39a128498c20ac0206a248]
    Former-commit-id: bfe4fe610a1d1dac5f7dbd182882b9e103f6f762

--- src/snakefiles/clean.py
+++ src/snakefiles/clean.py
@@ -1,29 +1,3 @@
 rule clean:
     shell:
-        "rm -r "
-            "{RAW_DIR} "
-            "{QC_DIR} "
-            "{NORM_DIR} "
-            "{ASSEMBLY_DIR} "
-
-
-
-rule clean_raw:
-    shell:
-        "rm -r {RAW_DIR} "
-
-
-
-rule clean_qc:
-    shell:
-        "rm -r {QC_DIR} "
-
-
-
-rule clean_diginorm:
-    shell:
-        "rm -r {NORM_DIR} "
-
-rule clean_assembly:
-    shell:
-        "rm -r {ASSEMBLY_DIR} "
+        "rm -r results"
