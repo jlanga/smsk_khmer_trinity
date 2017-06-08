d4a89c2a2400e984bd74c27cc9bf5cb5222f8226
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-06-08 16:55:14 +0200

    Adding a waiting time
    Former-commit-id: ef7a7c964c120687864d0cc11b0a3ee86bf802bd [formerly ef7a7c964c120687864d0cc11b0a3ee86bf802bd [formerly fa039394e7a3bd61548444155c8fe376f5f5e042]]
    Former-commit-id: 4b6a67d0de3522a3b6d21283ff90df6dc30d40af
    Former-commit-id: f1e804b1e900277fbb990a51c273efa3fdbca37a

--- src/snakefiles/assembly.py
+++ src/snakefiles/assembly.py
@@ -23,7 +23,7 @@ rule assembly_split_pe_files:
             "--output-first >(pigz --best > {output.left}) "
             "--output-second >(pigz --best > {output.right}) "
             "{input.fastq_pe} "
-        "> {log} 2>&1"
+        "> {log} 2>&1 ; sleep 5"
 
 rule assembly_merge_right_and_left:
     """
