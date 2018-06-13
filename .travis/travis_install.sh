760b6bcac7ad84a1bc3eee0a4c8480c01e590d36
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-06-13 16:23:43 +0200

    Travis #9

--- .travis/travis_install.sh
+++ .travis/travis_install.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
+export PATH="$HOME"/miniconda3_"$TRAVIS_OS_NAME"/bin:"$PATH"
+echo $PATH
+
+conda config --add channels defaults
+conda config --add channels conda-forge
+conda config --add channels bioconda
+
+conda install --yes --channel bioconda snakemake
